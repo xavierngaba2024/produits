1b382139337cd76a38d199154a38d738a27bf3dd
xavier_ng <xavierngaba@gmail.com>
2023-05-20 01:40:33 +0200

    classe de test est opérationnelle

--- src/test/java/com/xavier/produits/ProduitsApplicationTests.java
+++ src/test/java/com/xavier/produits/ProduitsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.xavier.produits;
 
 import java.util.Date;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,32 @@ class ProduitsApplicationTests {
 	public void testCreateProduit() {
 		Produit prod = new Produit("PC Dell",2200.500, new Date());
 		produitRepository.save(prod);
+	}
+	
+	@Test
+	public void testFindAllProduits() {
+		List<Produit> produits = produitRepository.findAll();
+		for(Produit p : produits)
+			System.out.println(p);
+	}
+	
+	@Test
+	public void testFindProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		System.out.println(p);
+	}
+	
+	@Test
+	public void testUpdateProduit() {
+		Produit p = produitRepository.findById(1L).get();
+		p.setPrixProduit(2000.00);
+		produitRepository.save(p);
+		System.out.println(p);
+	}
+	
+	@Test
+	public void testDeleteProduit() {
+		produitRepository.deleteById(1L);
 	}
 
 }

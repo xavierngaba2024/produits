69cc1d95044d9b7f9770031e69b754c4211a052b
xavier_ng <xavierngaba@gmail.com>
2023-05-27 01:22:57 +0200

    ajout de Lombock dans les classes Categorie et Produit

--- src/main/java/com/xavier/produits/entities/Categorie.java
+++ src/main/java/com/xavier/produits/entities/Categorie.java
@@ -1,5 +1,6 @@
 package com.xavier.produits.entities;
 
+import java.util.Date;
 import java.util.List;
 
 import jakarta.persistence.Entity;
@@ -7,7 +8,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Categorie {
 	
@@ -20,33 +27,5 @@ public class Categorie {
 	@OneToMany(mappedBy = "categorie")
 	private List<Produit> produits;
 	
-	
-	public Categorie() {
-		super();
-	}
-	public Long getIdCat() {
-		return idCat;
-	}
-	public void setIdCat(Long idCat) {
-		this.idCat = idCat;
-	}
-	public String getNomCat() {
-		return nomCat;
-	}
-	public void setNomCat(String nomCat) {
-		this.nomCat = nomCat;
-	}
-	public String getDescriptionCat() {
-		return descriptionCat;
-	}
-	public void setDescriptionCat(String descriptionCat) {
-		this.descriptionCat = descriptionCat;
-	}
-	public List<Produit> getProduits() {
-		return produits;
-	}
-	public void setProduits(List<Produit> produits) {
-		this.produits = produits;
-	}
 
 }
--- src/main/java/com/xavier/produits/entities/Produit.java
+++ src/main/java/com/xavier/produits/entities/Produit.java
@@ -7,7 +7,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Produit {
 
@@ -21,57 +27,11 @@ public class Produit {
 	@ManyToOne
 	private Categorie categorie;
 	
-	
-	public Produit() {
-		super();
-	}
-	
 	public Produit(String nomProduit, Double prixProduit, Date dateCreation) {
 		super();
 		this.nomProduit = nomProduit;
 		this.prixProduit = prixProduit;
 		this.dateCreation = dateCreation;
-	}
-	
-	public Long getIdProduit() {
-		return idProduit;
-	}
-
-	public void setIdProduit(Long idProduit) {
-		this.idProduit = idProduit;
-	}
-
-	public String getNomProduit() {
-		return nomProduit;
-	}
-	public void setNomProduit(String nomProduit) {
-		this.nomProduit = nomProduit;
-	}
-	public Double getPrixProduit() {
-		return prixProduit;
-	}
-	public void setPrixProduit(Double prixProduit) {
-		this.prixProduit = prixProduit;
-	}
-	public Date getDateCreation() {
-		return dateCreation;
-	}
-	public void setDateCreation(Date dateCreation) {
-		this.dateCreation = dateCreation;
-	}
-
-	public Categorie getCategorie() {
-		return categorie;
-	}
-
-	public void setCategorie(Categorie categorie) {
-		this.categorie = categorie;
-	}
-
-	@Override
-	public String toString() {
-		return "Produit [idProduit=" + idProduit + ", nomProduit=" + nomProduit + ", prixProduit=" + prixProduit
-				+ ", dateCreation=" + dateCreation + "]";
 	}
 	
 }

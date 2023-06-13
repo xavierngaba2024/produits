1e3427252d6ee19d05dede6d0a1059457a9a95e9
xavier_ng <xavierngaba@gmail.com>
2023-06-13 14:32:35 +0200

    Ajouter les méthodes du Repository à la couche Service

---
+++ src/main/java/com/xavier/produits/restcontrollers/ProduitRESTController.java
@@ -0,0 +1,27 @@
+package com.xavier.produits.restcontrollers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xavier.produits.entities.Produit;
+import com.xavier.produits.service.ProduitService;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class ProduitRESTController {
+	
+	@Autowired
+	ProduitService produitService;
+	
+	@GetMapping
+	List<Produit> getAllProduits() {
+		return produitService.getAllProduits();
+	}
+
+}

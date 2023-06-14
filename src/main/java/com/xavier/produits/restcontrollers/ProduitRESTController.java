5ec7432b539d95006e71a39c496fca482ad40ed7
xavier_ng <xavierngaba@gmail.com>
2023-06-14 21:49:21 +0200

    Créer le Web service REST permettant de consulter un produit

--- src/main/java/com/xavier/produits/restcontrollers/ProduitRESTController.java
+++ src/main/java/com/xavier/produits/restcontrollers/ProduitRESTController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,11 @@ public class ProduitRESTController {
 	@GetMapping
 	List<Produit> getAllProduits() {
 		return produitService.getAllProduits();
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Produit getProduitById(@PathVariable("id") Long id) {
+		return produitService.getProduit(id);
 	}
 
 }
